9677f70811a604d4c6c292c219c2725e801bf1e1
Taylor Reiter <taylorreiter@gmail.com>
2022-11-01 10:19:11 -0400

    rip params fasta

--- lib/WorkflowSeqqc.groovy
+++ lib/WorkflowSeqqc.groovy
@@ -12,10 +12,6 @@ class WorkflowSeqqc {
     public static void initialise(params, log) {
         
 
-        if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-            System.exit(1)
-        }
     }
 
     //
